61795c867eb75a0a52c431e29e6e4fe3df38a5b3
Jhohells <jhohellserick@gmail.com>
2024-01-17 23:33:35 -0500

    card weather ok

--- app/src/main/java/com/example/cardweather/MainActivity.kt
+++ app/src/main/java/com/example/cardweather/MainActivity.kt
@@ -1,8 +1,18 @@
 package com.example.cardweather
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextClock
+import android.widget.TextView
 import com.example.cardweather.databinding.ActivityMainBinding
+
+private const val ANIMATION_DURATION = 1000L
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +21,81 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonAgain.setOnClickListener {
+            moveTwoText(binding.textTitle, binding.textSubtitle)
+            binding.textTemperature.fadeInAndMoveUpAndChanged()
+            binding.imageWeather.fadeInAndMoveUp()
+        }
+
+    }
+
+    private fun moveTwoText(textView1: TextView, textView2: TextView) {
+        textView1.alpha = 0f
+        textView2.alpha = 0f
+
+        val fadeInAnimator1 = ObjectAnimator.ofFloat(textView1, "alpha", 0f, 1f)
+        fadeInAnimator1.duration = ANIMATION_DURATION
+        val fadeInAnimator2 = ObjectAnimator.ofFloat(textView2, "alpha", 0f, 1f)
+        fadeInAnimator2.duration = ANIMATION_DURATION
+
+        val animator1 = ObjectAnimator.ofFloat(textView1, "translationY", 0f, 10f)
+        animator1.duration = ANIMATION_DURATION
+        val animator2 = ObjectAnimator.ofFloat(textView2, "translationY", 0f, 10f)
+        animator2.duration = ANIMATION_DURATION
+
+        animator1.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                animator2.start()
+                fadeInAnimator2.start()
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+            }
+
+        })
+
+        fadeInAnimator1.start()
+        animator1.start()
+    }
+
+    private fun TextView.fadeInAndMoveUpAndChanged() {
+        alpha = 0f
+        translationY = 0f
+
+        val fadeInAnimator = ObjectAnimator.ofFloat(this, "alpha", 0f, 1f)
+        fadeInAnimator.duration = ANIMATION_DURATION
+
+        val moveUpAnimator = ObjectAnimator.ofFloat(this, "translationY", 0f, 10f)
+        moveUpAnimator.duration = ANIMATION_DURATION
+
+        val valueAnimator = ValueAnimator.ofInt(0, 31)
+        valueAnimator.duration = ANIMATION_DURATION
+        valueAnimator.addUpdateListener {
+            val animatedValue = it.animatedValue as Int
+            text = "$animatedValue°"
+        }
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playTogether(fadeInAnimator, moveUpAnimator, valueAnimator)
+        animatorSet.start()
+    }
+
+    private fun ImageView.fadeInAndMoveUp() {
+        val fadeInAnimator = ObjectAnimator.ofFloat(this, "alpha", 0f, 1f)
+        fadeInAnimator.duration = ANIMATION_DURATION
+
+        val translateAnimator = ObjectAnimator.ofFloat(this, "translationY", 0f, -20f)
+        fadeInAnimator.duration = ANIMATION_DURATION
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playTogether(fadeInAnimator, translateAnimator)
+        animatorSet.start()
     }
 }
